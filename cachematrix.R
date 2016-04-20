5a78ec6ce54f0f1670e0ca1376915f8503ff8342
ctctctct <cameron.turner@meggitt.com>
2016-04-20 10:32:22 +0100

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are 2 main functions, one takes a matrix and caches it and outputs a list of other functions. The second then checks to see if it the inverse is cached, returns if it is, calculates, stores and the returns if not.
 
-## Write a short comment describing this function
+## Creates a list of functions to be passed on to the cachesolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) { ## The set function, it changes what matrix has been stored.
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x  ## The get function, this retrieves the stored matrix.
+        setinverse <- function(solve) m <<- solve ##The setinverse function, this sets the inverse of the matrix.
+        getinverse <- function() m ##The getinverse function, this retrieves the inverse of the matrix.
+        list(set = set, get = get, ## This is the output of the function which is a list of the functions created in this function
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## It checks to see if the inverse is in the cache, if it is it returns it, if not it calculates it, stores and the then returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse() #This tries looks to retrieve the inverse from the cache and assign it to m
+        if(!is.null(m)) {  #If m is not null then it returns m and returns the message getting cached data
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()  # If m is null then it retrieves the matrix from the cache
+        m <- solve(data, ...) #Solves it
+        x$setinverse(m) #And sets the inverse in the cache
+        m #It then returns the inverse
 }
